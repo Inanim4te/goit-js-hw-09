035e8ef314bf675c3dd9298d0ac2f1bebc4924f1
Inanim4te <Obsidianuz689@gmail.com>
2023-02-07 03:12:28 +0200

    did hw

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,43 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+Notify.init({
+  useIcon: false,
+});
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+
+  return new Promise((res, rej) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        res({ position, delay });
+      } else {
+        rej({ position, delay });
+      }
+    }, delay);
+  });
 }
+
+const firstDelay = document.querySelector('input[name="delay"]');
+const step = document.querySelector('input[name="step"]');
+const amount = document.querySelector('input[name="amount"]');
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', e => {
+  for (let i = 1; i <= amount.value; i++) {
+    let totalDelay =
+      Number(firstDelay.value) + (Number(step.value) * i - Number(step.value));
+    if (totalDelay <= 0) {
+      totalDelay = 0;
+    }
+    createPromise(i, totalDelay)
+      .then(({ position, delay }) => {
+        Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+  }
+  e.currentTarget.reset();
+  e.preventDefault();
+});
